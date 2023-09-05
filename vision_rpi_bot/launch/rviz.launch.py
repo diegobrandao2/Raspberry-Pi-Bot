26568927cf3accd0aac6310fa9b5b654bddf20d3
diegobrandao2 <diegobrandao2@gmail.com>
2023-09-05 13:41:19 +1200

    adding camera plugin

--- vision_rpi_bot/launch/rviz.launch.py
+++ vision_rpi_bot/launch/rviz.launch.py
@@ -15,8 +15,8 @@ def generate_launch_description():
         ),
 
         Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
             name='jsp_node'
         ),
 
